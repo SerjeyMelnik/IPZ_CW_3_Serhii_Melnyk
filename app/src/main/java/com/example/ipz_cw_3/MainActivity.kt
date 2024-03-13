cff11f62b69489ffe8664e6932747449f641dedb
Serhii Melnyk <serjeymelnik@gmail.com>
2024-03-13 14:19:51 +0200

    refactor to main screen

--- app/src/main/java/com/example/ipz_cw_3/MainActivity.kt
+++ app/src/main/java/com/example/ipz_cw_3/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.ipz_cw_3.ui.theme.IPZ_CW_3_Serhii_MelnykTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +25,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             IPZ_CW_3_Serhii_MelnykTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +33,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun MainScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+
+    }
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "30 day of wellness",
+        modifier = modifier, style = TextStyle(fontSize = 32.sp, color = Color.Black)
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    IPZ_CW_3_Serhii_MelnykTheme {
-        Greeting("Android")
-    }
+fun MainScreenPreview() {
+    IPZ_CW_3_Serhii_MelnykTheme { MainScreen() }
 }
